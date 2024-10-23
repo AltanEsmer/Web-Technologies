264bd6fd2726be53786a4786d523ce04aafbea04
botilla35 <botilla35@gmail.com>
2024-10-23 11:44:18 +0200

    RememberMe functionality to login page

--- Container/script.js
+++ Container/script.js
@@ -1,9 +1,31 @@
-const logreBox = document.querySelector('.logreg-box')
-const loginLink = document.querySelector('.login-link')
-const registerLink = document.querySelector('.register-link')
+const logreBox = document.querySelector('.logreg-box');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
 
-// Dilara burdaydı
+//for rememberMe function
+const rmCheck = document.getElementById("checker");
+const emailInput = document.getElementById("mail");
+const signInBtn = document.getElementById("sign-in-btn");
 
+if (localStorage.checkbox && localStorage.checkbox !== "") {
+rmCheck.setAttribute("checked", "checked");
+emailInput.value = localStorage.username;
+} else {
+rmCheck.removeAttribute("checked");
+emailInput.value = "";
+}
+
+function lsRememberMe() {
+if (rmCheck.checked && emailInput.value !== "") {
+  localStorage.username = emailInput.value;
+  localStorage.checkbox = rmCheck.value;
+} else {
+  localStorage.username = "";
+  localStorage.checkbox = "";
+}
+}
+
+//for sign in/up page shift
 registerLink.addEventListener('click', () => {
   logreBox.classList.add('active');
 });
@@ -11,3 +33,5 @@ registerLink.addEventListener('click', () => {
 loginLink.addEventListener('click', () => {
   logreBox.classList.remove('active');
 });
+
+//for checking if input data is valid
