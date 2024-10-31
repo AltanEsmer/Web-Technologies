643ff0297fab95e35c5ce59a96fac43e40aaa7aa
Vedat-Esendag <91906898+Vedat-Esendag@users.noreply.github.com>
2024-10-31 12:42:09 +0100

    laravel fix

--- musicLibrary/routes/web.php
+++ musicLibrary/routes/web.php
@@ -2,6 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route for the Sign In page
+Route::get('/signin', function () {
+    return view('signin');
+})->name('signin');
+
+// Route for the Home page
+Route::get('/home', function () {
+    return view('home');
+})->name('home');
+
+// Route for the Library page
+Route::get('/library', function () {
+    return view('library');
+})->name('library');
+
+// Set the default route to Sign In if you want the root URL to show the Sign In page
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('signin');
+})->name('default');
