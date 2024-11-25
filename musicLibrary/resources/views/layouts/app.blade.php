907bb23a903e4da49e9862e8cdcf4285efce3acd
Altan Esmer <esmeraltan@gmail.com>
2024-11-25 10:51:22 +0100

    Fixing layout of header and navbar
    I transferred my changes to this branch for avoid conflict issues

--- musicLibrary/resources/views/layouts/app.blade.php
+++ musicLibrary/resources/views/layouts/app.blade.php
@@ -11,28 +11,42 @@
 </head>
 <body class="bg-gray-100">
     <header class="header">
-        <nav class="navbar">
-            <div class="logo-image-wrapper">
-                <img src="{{ asset('images/MusicLibraryLogo.png') }}" alt="Logo" class="about-image">
-            </div> 
-            <a href="{{ route('home') }}">Home</a>
-            <a href="#about">About</a>
-            @auth
-                <a href="{{ route('library') }}">Library</a>
-                <form method="POST" action="{{ route('logout') }}" style="display: inline;">
-                    @csrf
-                    <button type="submit" style="background: none; border: none; color: white; cursor: pointer;">Logout</button>
+        <nav class="navbar flex justify-between items-center w-full">
+            <div class="flex items-center space-x-4 ml-0">
+                <div class="logo-image-wrapper pl-0">
+                    <img src="{{ asset('images/MusicLibraryLogo.png') }}" alt="Logo" class="about-image">
+                </div> 
+                @auth
+                    <a href="{{ route('library') }}" class="pl-0">Library</a>
+                @endauth
+            </div>
+            
+            <div class="flex items-center space-x-6">
+                <form action="#" class="search-bar">
+                    <input type="text" name="search" id="search" placeholder="Search...">
+                    <button type="submit"><i class='bx bx-search'></i></button>
                 </form>
-            @else
-                <a href="{{ route('signin') }}">Sign in</a>
-            @endauth
-            <a href="#contact">Contact</a>
+                
+                @auth
+                    <form method="POST" action="{{ route('logout') }}" class="inline">
+                        @csrf
+                        <a href="#" onclick="event.preventDefault(); this.closest('form').submit();">Logout</a>
+                    </form>
+                @else
+                    <a href="{{ route('signin') }}">Sign in</a>
+                @endauth
+            </div>
         </nav>
-        <form action="#" class="search-bar">
-            <input type="text" name="search" id="search" placeholder="Search...">
-            <button type="submit"><i class='bx bx-search'></i></button>
-        </form>
     </header>
+
+    <style>
+    .navbar {
+        padding-left: 0;
+    }
+    .logo-image-wrapper {
+        margin-left: 0;
+    }
+    </style>
 
     @yield('content')
 </body>
